5589cfa7751b4b04b39735fb6cbd0c810a602e3d
luisangeluis <luisangeluis@gmail.com>
2022-08-09 15:18:20 -0500

    Stylizing to the cards

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 const CardMovie = ({ item }) => {
   //TO DO GET THE IMAGE
   return (
-    <article className="card card-carousel">
-      <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="" />
+    <article
+      className="card card-carousel"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
+      }}
+    >
+      {/* <img
+        src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+        className="img-fluid"
+        alt={item.original_title}
+      /> */}
       <div className="card-body">
-        <h5 className="">{item.title}</h5>
-        <p className="">{item.overview.substring(0, 100)}...</p>
+        <h5 className="card-title">{item.title}</h5>
+        {/* <p className="">{item.overview.substring(0, 100)}...</p> */}
       </div>
     </article>
   );
